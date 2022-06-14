73ad0a118beee3c1a4cc08318b7f71626793fff2
Yurii Selinnyi <quizcanners@gmail.com>
2022-06-14 11:56:32 +0300

    Туув Фееутешщт Ршпрдшпре

--- Inspector/Scripts/PlayerAndEditorGui_HierarchyWarnings.cs
+++ Inspector/Scripts/PlayerAndEditorGui_HierarchyWarnings.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_EDITOR
+
 using UnityEngine;
+using UnityEditor;
+using QuizCanners.Utils;
 
-public class PlayerAndEditorGui_HierarchyWarnings : MonoBehaviour
+namespace QuizCanners.Inspect
 {
-    // Start is called before the first frame update
-    void Start()
+    [InitializeOnLoad]
+    public class PlayerAndEditorGui_HierarchyWarnings
     {
-        
-    }
+        static PlayerAndEditorGui_HierarchyWarnings()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += HierarchyItemCB;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        static void HierarchyItemCB(int instanceID, Rect selectionRect)
+        {
+            GameObject go = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+            if (!go)
+                return;
+
+            var na = go.GetComponents<INeedAttention>();
+            string msg = "";
+
+            foreach (var a in na)
+            {
+                if (a.TryGetAttentionMessage(out var currentMessage))
+                    msg += currentMessage;
+            }
+
+            if (msg.IsNullOrEmpty())
+                return;
+
+            Rect r = new Rect(selectionRect)
+            {
+                x = 30,
+                width = 18
+            };
+
+            GUIContent cnt = new GUIContent(Icon.Warning.GetIcon(), msg);
+            GUI.Label(r, cnt);
+        }
     }
 }
+#endif
